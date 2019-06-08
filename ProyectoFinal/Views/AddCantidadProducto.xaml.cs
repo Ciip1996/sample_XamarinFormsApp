ddd4ec2ff13c99efbe03981e5fbaafe2aead2f08
Carlos Aviña <carlosavilu11@gmail.com>
2019-06-08 17:01:25 -0500

    AddCantidadProduct y AddInfoProducto vistas añadidas

---
+++ ProyectoFinal/Views/AddCantidadProducto.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using ProyectoFinal.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.MultiSelectListView;
+
+namespace ProyectoFinal.Views
+{
+    public partial class AddCantidadProducto : ContentPage
+    {
+        MultiSelectObservableCollection<Product> listaProductos = new MultiSelectObservableCollection<Product>();
+        public AddCantidadProducto(MultiSelectObservableCollection<Product> _list)
+        {
+            InitializeComponent();
+            listaProductos = _list;
+        }
+
+        private void btnMas_Clicked(object sender, System.EventArgs e)
+        {
+            //Button button = sender as Button;
+            //int index = listaProductos.IndexOf(button);
+
+            Console.WriteLine("0");
+        }
+
+        void btnMenos_Clicked(object sender, System.EventArgs e)
+        {
+            //if(cantidad - 1 >= 0)
+            //{
+            //    cantidad--;
+            //    //txtCant.Text = cantidad.ToString();
+            //}
+            Console.WriteLine("0");
+        }
+
+        private async void btnAddInfo_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new AddInfoProducto());
+        }
+    }
+}
---
+++ ProyectoFinal/Views/AddInfoProducto.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Plugin.Geolocator;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace ProyectoFinal.Views
+{
+    public partial class AddInfoProducto : ContentPage
+    {
+        public AddInfoProducto()
+        {
+            InitializeComponent();
+
+            MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(21.1576191, -101.6975641), Distance.FromMiles(1)));
+
+            getLocationAsync();
+        }
+
+        public async Task getLocationAsync()
+        {
+            try
+            {
+                var location = await Geolocation.GetLocationAsync();
+
+                if (location != null)
+                {
+                    Xamarin.Forms.Maps.Pin pin = new Pin()
+                    {
+                        Type = PinType.Place,
+                        Position = new Position(location.Latitude, location.Longitude),
+                        Label = "Plaza Mayor",
+                        Address = "Boulevard Juan Alonso de Torres Poniente, Valle del Campestre, León, Guanajuato",
+                        Id = "Xamarin"
+                    };
+
+                    MyMap.Pins.Add((Xamarin.Forms.Maps.Pin)pin);
+                    MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(location.Latitude, location.Longitude), Distance.FromMiles(1.0)));
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        public bool IsLocationAvailable()
+        {
+            if (!CrossGeolocator.IsSupported)
+                return false;
+
+            return CrossGeolocator.Current.IsGeolocationAvailable;
+        }
+    }
+}
