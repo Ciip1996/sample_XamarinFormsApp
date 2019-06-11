c63e7896fd7f47af3017d2f0d4d084238db4ea31
Carlos Aviña <carlosavilu11@gmail.com>
2019-06-11 02:26:04 -0500

    View pedidos a medias :v

---
+++ ProyectoFinal/Views/Mapa.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+
+namespace ProyectoFinal.Views
+{
+    public partial class Mapa : ContentPage
+    {
+        public Mapa(double latitud, double longitud)
+        {
+            InitializeComponent();
+            MapaEntrega.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(latitud, longitud), Distance.FromMiles(1)));
+        }
+    }
+}
--- ProyectoFinal/Views/Pedidos.xaml.cs
+++ ProyectoFinal/Views/Pedidos.xaml.cs
@@ -10,6 +10,8 @@ namespace ProyectoFinal.Views
     public partial class Pedidos : ContentPage
     {
         Entrega_DataBase dataBase = new Entrega_DataBase(App.entrega_path);
+        List<Entrega> ClientsCollection = new List<Entrega>();
+
         public Pedidos()
         {
             InitializeComponent();
@@ -17,10 +19,46 @@ namespace ProyectoFinal.Views
 
         protected async override void OnAppearing()
         {
-            List<Entrega> ClientsCollection = new List<Entrega>();
-
             ClientsCollection = await dataBase.GetItemsAsync();
-            lstEntregas.ItemsSource = ClientsCollection;
+
+            string[] _list = new string[ClientsCollection.Count];
+            ClientsData client = new ClientsData();
+
+            for(int i = 0;i < ClientsCollection.Count;i++)
+            {
+                _list[i] = $"{ClientsCollection[i].id}\t{client.Clients[ClientsCollection[i].id].Name}\t{ClientsCollection[i].fecha_entrega}\t{ClientsCollection[i].estatus}";
+            }
+
+            lstEntregas.ItemsSource = null;
+            lstEntregas.ItemsSource = _list;
+        }
+
+        //async void Item_Tapped(object sender, System.EventArgs e)
+        //{
+        //    var id_entrega = lstEntregas.SelectedItem.ToString();
+
+        //    //await Navigation.PushAsync(new Mapa());
+        //}
+
+        public async void OnMapa(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+
+            var id = int.Parse(mi.CommandParameter.ToString()[0].ToString());
+            await Navigation.PushAsync(new Mapa(ClientsCollection[id].latitud, ClientsCollection[id].longitud));
+        }
+
+        public async void OnOpciones(object sender, EventArgs e)
+        {
+            var mi = ((MenuItem)sender);
+            var res = await DisplayAlert("¿Entregar?", mi.CommandParameter.ToString(), "OK", "Cancel");
+
+            if (res)
+            {
+                Console.WriteLine(mi.Id);
+                Console.WriteLine(mi.Text);
+                Console.WriteLine(mi.CommandParameter.ToString());
+            }
         }
     }
 }
